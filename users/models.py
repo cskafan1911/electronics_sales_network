5d6c2658e1f04f22237a42295e8901f28118c446
Dmitry Dronov <cskafan1911@gmail.com>
2024-05-04 21:27:50 +0300

    Реализована авторизация пользователя по simplejwt.

--- users/models.py
+++ users/models.py
@@ -6,4 +6,10 @@ class User(AbstractUser):
     """
     Класс для модели Пользователя.
     """
-    pass
+
+    username = models.CharField(max_length=50, unique=True, verbose_name='Имя для входа')
+    first_name = models.CharField(max_length=150, verbose_name='Имя пользователя', blank=True, null=True)
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия пользователя', blank=True, null=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
